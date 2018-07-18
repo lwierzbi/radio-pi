2915b0b3c461c9e597dd429dce68929da342f13a
Lukasz Wierzbicki <lucas@AMDC3772.digital.local>
2018-07-18 13:44:53 +0200

    test

---
+++ test.js
@@ -0,0 +1,22 @@
+var https = require('https');
+
+function main()
+{
+
+https.get('https://ozb5brulgk.execute-api.us-east-1.amazonaws.com/myDeployment/weather1?city=Warsaw', (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
+}).on('error', (e) => {
+  console.error(e);
+});
+
+
+}
+
+main()
+
